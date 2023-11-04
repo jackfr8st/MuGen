ec570e64cc3de11297213191b9a841c63619b373
Tavion20 <tavionalric@gmail.com>
2023-11-05 00:11:08 +0530

    functional

--- src/GenPage.js
+++ src/GenPage.js
@@ -50,9 +50,23 @@ function GenPage() {
 
   const getRImage = () => {
     setPlaying(true);
-    const rIndex = Math.floor(Math.random() * Images.length);
-    setPath(Images[rIndex]);
-  };
+    fetch("http://localhost:10000/runmodel", {
+      method: "POST",
+      headers: {'content-type': 'application/json'},
+      body: JSON.stringify({
+        genInfo:{
+          duration:10
+        }
+      })
+    })
+      .then((response) => response.arrayBuffer())
+      .then((response) => {
+        console.log(response);
+         setPath(response);
+        });
+      }
+    // const rIndex = Math.floor(Math.random() * Images.length);
+    // setPath(Images[rIndex]);
   // const [audio, setAudio] = useState<MediaStream | null>(null);
 
   // useEffect(() => {
@@ -67,89 +81,87 @@ function GenPage() {
   const canvasRef = useRef(null),
     buttonRef = useRef(null);
 
-  const audioVisualizerLogic = () => {
-    const context = new (window.AudioContext || window.webkitAudioContext)(),
-      source = context.createBufferSource();
+  // const audioVisualizerLogic = () => {
+  //   const context = new (window.AudioContext || window.webkitAudioContext)(),
+  //     source = context.createBufferSource();
 
-    //fetch remote audio source
-    fetch("https://jplayer.org/audio/mp3/RioMez-01-Sleep_together.mp3")
-      .then((response) => response.arrayBuffer())
-      .then((response) => {
-        context.decodeAudioData(response, (buffer) => {
-          source.buffer = buffer;
-          source.connect(context.destination);
-          // auto play
-          source.start(0);
-        });
-      });
+  //   // fetch remote audio source
+    
+  //       context.decodeAudioData(path, (buffer) => {
+  //         source.buffer = buffer;
+  //         source.connect(context.destination);
+  //         // auto play
+  //         source.start(0);
+  //       });
+      
 
-    const audio = new Audio(source),
-      canvas = canvasRef.current,
-      muteButton = buttonRef.current;
+  //   const audio = new Audio(source),
+  //     canvas = canvasRef.current,
+  //     muteButton = buttonRef.current;
 
-    //mute or play on click
-    const mutePlay = () => {
-      context.state === "running" ? context.suspend() : context.resume();
-    };
-    muteButton.onclick = () => mutePlay();
+  //   //mute or play on click
+  //   const mutePlay = () => {
+  //     context.state === "running" ? context.suspend() : context.resume();
+  //   };
+  //   muteButton.onclick = () => mutePlay();
 
-    //config canvas
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    const ctx = canvas.getContext("2d");
+  //   //config canvas
+  //   canvas.width = window.innerWidth;
+  //   canvas.height = window.innerHeight;
+  //   const ctx = canvas.getContext("2d");
 
-    //config audio analyzer
-    const analyser = context.createAnalyser();
-    source.connect(analyser);
-    analyser.connect(context.destination);
-    analyser.fftSize = 256;
-    const bufferLength = analyser.frequencyBinCount,
-      dataArray = new Uint8Array(bufferLength),
-      WIDTH = canvas.width,
-      HEIGHT = canvas.height,
-      barWidth = (WIDTH / bufferLength) * 2.5;
-    let barHeight = null,
-      x = null;
+  //   //config audio analyzer
+  //   const analyser = context.createAnalyser();
+  //   source.connect(analyser);
+  //   analyser.connect(context.destination);
+  //   analyser.fftSize = 256;
+  //   const bufferLength = analyser.frequencyBinCount,
+  //     dataArray = new Uint8Array(bufferLength),
+  //     WIDTH = canvas.width,
+  //     HEIGHT = canvas.height,
+  //     barWidth = (WIDTH / bufferLength) * 2.5;
+  //   let barHeight = null,
+  //     x = null;
 
-    //core logic for the visualizer
-    const timeouts = [];
-    const renderFrame = () => {
-      ctx.fillStyle = "rgba(0,0,0,0)";
-      requestAnimationFrame(renderFrame);
-      x = 0;
-      analyser.getByteFrequencyData(dataArray);
-      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+  //   //core logic for the visualizer
+  //   const timeouts = [];
+  //   const renderFrame = () => {
+  //     ctx.fillStyle = "rgba(0,0,0,0)";
+  //     requestAnimationFrame(renderFrame);
+  //     x = 0;
+  //     analyser.getByteFrequencyData(dataArray);
+  //     ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
-      for (let i = 0; i < bufferLength; i++) {
-        //color based upon frequency
-        barHeight = dataArray[i];
-        let r = barHeight * 1,
-          g = 0,
-          b = barHeight * 4.5;
-        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
-        x += barWidth + 1;
+  //     for (let i = 0; i < bufferLength; i++) {
+  //       //color based upon frequency
+  //       barHeight = dataArray[i];
+  //       let r = barHeight * 1,
+  //         g = 0,
+  //         b = barHeight * 4.5;
+  //       ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+  //       ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+  //       x += barWidth + 1;
 
-        //Allows visualizer to overlay on a background/video by clearing the rects after painting.
-        let timer = setTimeout(() => {
-          ctx.clearRect(0, 0, WIDTH, HEIGHT);
-        }, 50);
-        timeouts.push(timer);
-      }
-    };
-    //Clears the accumulating timeouts.
-    setTimeout(() => {
-      for (let i = 0; i < timeouts.length; i++) {
-        return clearTimeout(timeouts[i]);
-      }
-    }, 51);
-    renderFrame();
-  };
+  //       //Allows visualizer to overlay on a background/video by clearing the rects after painting.
+  //       let timer = setTimeout(() => {
+  //         ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  //       }, 50);
+  //       timeouts.push(timer);
+  //     }
+  //   };
+  //   //Clears the accumulating timeouts.
+  //   setTimeout(() => {
+  //     for (let i = 0; i < timeouts.length; i++) {
+  //       return clearTimeout(timeouts[i]);
+  //     }
+  //   }, 51);
+  //   renderFrame();
+  // };
 
-  //connect audio visualizer to DOM and execute logic
-  useEffect(() => {
-    audioVisualizerLogic();
-  }, []);
+  // //connect audio visualizer to DOM and execute logic
+  // useEffect(() => {
+  //   audioVisualizerLogic();
+  // }, []);
 
   //test
   //   const canvasRef = useRef(null);
@@ -304,7 +316,7 @@ function GenPage() {
                 {isPlaying ? "Pause" : "Play"}
               </button> */}
               {/* <AudioPlayer src={midipath} autoPlay={isPlaying} controls style={{backgroundColor:'black'}} /> */}
-              <MidiPlayer src={midipath} />
+              <MidiPlayer data={path} />
 
               <div
                 style={{
